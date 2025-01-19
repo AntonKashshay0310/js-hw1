baf603bcaad66e2a738dc3cbc964775a0cac281d
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-01-19 19:35:14 +0200

    add

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+const age = 13;
+console.log(age)
+
+const name = 'Anton';
+console.log(name)
+
+const isStudent = true;
+console.log(isStudent)
+
+const myString = 'Найкраща помста — величезний успіх';
+console.log(myString)
+
+const myNumber = 27;
+console.log(myNumber + 10)
+
+const myNull = null;
+console.log(myNull)
