5e58b4dc2511f3f8cc57b97d658641bca78b5f40
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-01-20 19:43:38 +0200

    add

--- js/script1.js
+++ js/script1.js
@@ -1,17 +1,24 @@
+// 1
 const age = 13;
 console.log(age)
-
+// 2
 const name = 'Anton';
 console.log(name)
-
+// 3
 const isStudent = true;
 console.log(isStudent)
-
+// 4
 const myString = 'Найкраща помста — величезний успіх';
 console.log(myString)
-
+// 5
 const myNumber = 27;
 console.log(myNumber + 10)
-
+// 6
 const myNull = null;
 console.log(myNull)
+// 7
+const userName = prompt("Whar is your name?");
+console.log(userName)
+// 8
+const userAnswer = confirm('Ви дійсно хочете покинути сторінку?')
+if (userAnswer)
